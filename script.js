7a2cdbd4954837206216e359d563dc99763b8905
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2022-08-16 12:13:52 +0530

    added joke api to get random jokes

--- script.js
+++ script.js
@@ -107,17 +107,26 @@ const VoiceRSS = {
   },
 };
 
-function test() {
-  VoiceRSS.speech({
-    key: "f02fae0ff0824539937c736289288e34",
-    src: "Hello, world!",
-    hl: "en-us",
-    v: "Linda",
-    r: 0,
-    c: "mp3",
-    f: "44khz_16bit_stereo",
-    ssml: false,
-  });
+
+
+// Get jokes from Joke API
+async function getJokes() {
+    let joke;
+    const apiUrl = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if (data.setup) {
+            joke = `${data.setup}.... ${data.delivery}`
+        } else {
+            joke = data.joke;
+        }
+        tellMe(joke);
+    } catch (error) {
+        // Catch error here
+        console.log('whoops', error);
+    }
 }
 
-test();
+getJokes();
